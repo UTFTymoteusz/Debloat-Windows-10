6bec5b20c7b99822237cd3caae0cca6c06f434f6
UTFTymoteusz <tymoteusz.paliwoda@gmail.com>
2020-05-13 13:38:55 +0200

    Create toolazy.ps1

---
+++ toolazy.ps1
@@ -0,0 +1,16 @@
+.\scripts\remove-onedrive.ps1
+.\scripts\block-telemetry.ps1
+.\scripts\disable-services.ps1
+.\scripts\disable-windows-defender.ps1
+.\scripts\fix-privacy-settings.ps1
+.\scripts\optimize-user-interface.ps1
+.\scripts\optimize-windows-update.ps1
+.\scripts\remove-default-apps.ps1
+
+.\utils\enable-god-mode.ps1
+
+.\utils\disable-prefetch-prelaunch.ps1
+.\utils\disable-scheduled-tasks.ps1
+.\utils\disable-ShellExperienceHost.bat
+
+.\scripts\experimental_unfuckery.ps1
